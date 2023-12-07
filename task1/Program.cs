69554a0d64b2a50c090e7bb49b52fe5387dc63dc
Pakker <gfabinam@gmail.com>
2023-12-07 19:24:51 +0300

    1

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+
+
+void InifinityQ()
+{
+    while (true)
+    {
+        Console.Write("Введите число: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num * 2 % 2 == 0)
+        {
+            Console.WriteLine("[STOP]");
+            break;
+        }
+
+
+    }
+
+}
+
+InifinityQ();
