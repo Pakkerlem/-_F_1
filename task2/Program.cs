3ebda0dace97da33e946534b42236b63542b0521
Pakker <gfabinam@gmail.com>
2023-12-07 19:47:37 +0300

    3

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,20 +1,23 @@
 ﻿// Задайте массив заполненный случайными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
-
-Random random = new Random();
-int arrayLength = 100;
-
-int[] array = new int[arrayLength];
-
-for (int i = 0; i < arrayLength; i++)
+void task1()
 {
-    array[i] = new Random().Next(100, 1000);
-}
-int count = 0;
-foreach (int num in array)
-{
-    if (num % 2 == 0)
+    Random random = new Random();
+    int arrayLength = 100;
+
+    int[] array = new int[arrayLength];
+
+    for (int i = 0; i < arrayLength; i++)
     {
-        count++;
+        array[i] = new Random().Next(100, 1000);
     }
+    int count = 0;
+    foreach (int num in array)
+    {
+        if (num % 2 == 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Количетсво четных чисел в массиве: {count} ");
 }
-Console.WriteLine($"Количетсво четных чисел в массиве: {count} ");
+task1();
