3ed622e6e31497611602df4474794487a244837c
Pakker <gfabinam@gmail.com>
2023-12-10 02:22:05 +0300

    4

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,21 +3,53 @@
 
 
 
-void InifinityQ()
+
+
+void Main()
 {
     while (true)
     {
-        Console.Write("Введите число: ");
-        int num = Convert.ToInt32(Console.ReadLine());
-        if (num * 2 % 2 == 0)
+        Console.WriteLine("Введите целое число или 'q' для выхода:");
+        string input = Console.ReadLine();
+
+        if (input.ToLower() == "q")
         {
-            Console.WriteLine("[STOP]");
+            Console.WriteLine("Выход из программы...");
             break;
         }
 
+        int number;
+        bool isNumber = int.TryParse(input, out number);
 
+        if (isNumber)
+        {
+            int sum = CalculateSumOfDigits(number);
+            if (sum % 2 == 0)
+            {
+                Console.WriteLine($"Сумма цифр числа {number} чётная. Программа завершена.");
+                break;
+            }
+            else
+            {
+                Console.WriteLine($"Сумма цифр числа {number} нечётная.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Неверный формат ввода. Пожалуйста, введите целое число или 'q' для выхода.");
+        }
     }
-
 }
 
-InifinityQ();
+int CalculateSumOfDigits(int number)
+{
+    int sum = 0;
+    while (number != 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+Main();
