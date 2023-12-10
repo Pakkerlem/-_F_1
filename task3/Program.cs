d7e7a7ab78d48afd3c5ccabe74da12d31e7d8cce
Pakker <gfabinam@gmail.com>
2023-12-10 18:27:34 +0300

    6

---
+++ task3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+
+
+static void task3()
+{
+    Random random = new Random();
+
+    int arrayLength = 100;
+
+    int[] array = new int[arrayLength];
+
+
+    for (int i = 0; i>arrayLength; i++)
+    {
+        array[i] = random.Next(10, 100);
+        Console.Write(array[i] + " ");
+    } 
+    // for (int i = 0; i>arrayLength/2; i++)
+    // {
+    //     array[i] = array[arrayLength-i];
+    // }
+    // for (int i = 0; i>arrayLength; i++)
+    // {
+    //     Console.Write($"F {array[i]}");
+    // }
+
+    
+
+}
+task3();
