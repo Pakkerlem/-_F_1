686d0dc5921f0f929a5537803fd11241039e47e6
Pakker <gfabinam@gmail.com>
2023-12-10 18:33:52 +0300

    7

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,30 +1,51 @@
 ﻿// Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
 
 
-static void task3()
-{
-    Random random = new Random();
-
-    int arrayLength = 100;
-
-    int[] array = new int[arrayLength];
 
 
-    for (int i = 0; i>arrayLength; i++)
+    static void task3()
     {
-        array[i] = random.Next(10, 100);
-        Console.Write(array[i] + " ");
-    } 
-    // for (int i = 0; i>arrayLength/2; i++)
-    // {
-    //     array[i] = array[arrayLength-i];
-    // }
-    // for (int i = 0; i>arrayLength; i++)
-    // {
-    //     Console.Write($"F {array[i]}");
-    // }
+        // Задаем длину массива
+        int arrayLength = 10;
+        
+        // Создаем массив с рандомными числами
+        int[] randomArray = new int[arrayLength];
+        Random random = new Random();
+        
+        Console.WriteLine("Исходный массив со случайными числами:");
+        for (int i = 0; i < arrayLength; i++)
+        {
+            randomArray[i] = random.Next(1, 101); 
+            Console.Write(randomArray[i] + " ");
+        }
 
-    
+        ReverseArray(randomArray); 
 
-}
+        Console.WriteLine("\n\nПеревернутый массив:");
+        foreach (int num in randomArray)
+        {
+            Console.Write(num + " ");
+        }
+    }
+
+    static void ReverseArray(int[] arr)
+    {
+        int left = 0;
+        int right = arr.Length - 1;
+
+        while (left < right)
+        {
+            
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            
+            left++;
+            right--;
+        }
+    }
 task3();
+
+
+
